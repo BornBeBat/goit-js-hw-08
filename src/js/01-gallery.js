81dd1e655c2f7f88e70ae2d56ff5b210ed29cbf7
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-22 17:19:09 +0200

    1-task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const marcupConteiner = document.querySelector('.gallery');
+const cardsMarcup = createMarcup(galleryItems);
+
+marcupConteiner.insertAdjacentHTML('beforeend', cardsMarcup);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '400',
+});
+
+function createMarcup(images) {
+  const marcup = images.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+  });
+  return marcup.join('');
+}
