1f93dddc4625c138117d5eee08d57e5136ef78e4
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-23 12:44:34 +0200

    Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Player(iframe);
+let videoplayer_current_time = localStorage.getItem('videoplayer_current_time');
+
+player.setCurrentTime(videoplayer_current_time);
+
+const onPlay = function ({ seconds }) {
+  console.log(seconds);
+  localStorage.setItem('videoplayer_current_time', `${Math.floor(seconds)}`);
+};
+
+const onTrottledPlay = throttle(onPlay, 1000, { trailing: true });
+player.on('timeupdate', onTrottledPlay);
